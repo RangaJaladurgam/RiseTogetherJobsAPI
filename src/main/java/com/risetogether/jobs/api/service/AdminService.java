9ae7574fe75cfeec7c1cf69e678c6a1792fa6700
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-18 15:57:09 +0530

    Working saveAdmin in AdminService

--- src/main/java/com/risetogether/jobs/api/service/AdminService.java
+++ src/main/java/com/risetogether/jobs/api/service/AdminService.java
@@ -2,7 +2,28 @@ package com.risetogether.jobs.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.risetogether.jobs.api.entity.Admin;
+import com.risetogether.jobs.api.mapper.AdminMapper;
+import com.risetogether.jobs.api.repository.AdminRepository;
+import com.risetogether.jobs.api.requestdto.AdminRequest;
+import com.risetogether.jobs.api.responsedto.AdminResponse;
+
 @Service
 public class AdminService {
+	
+	private final AdminRepository adminRepository;
+	private final AdminMapper adminMapper; 
+	
+	public AdminService(AdminRepository adminRepository, AdminMapper adminMapper) {
+		super();
+		this.adminRepository = adminRepository;
+		this.adminMapper = adminMapper;
+	}
+
+	public AdminResponse saveAdmin(AdminRequest adminRequest) {
+		Admin admin = adminMapper.mapToAdmin(adminRequest, new Admin());
+		admin = adminRepository.save(admin);
+		return adminMapper.mapToAdminRespone(admin);
+	}
 	
 }
