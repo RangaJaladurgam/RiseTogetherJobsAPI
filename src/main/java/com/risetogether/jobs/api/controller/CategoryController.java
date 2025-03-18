f843efc703154aaf07f8070a559f369c04458ba9
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-18 19:07:01 +0530

    Creating CategoryService

--- src/main/java/com/risetogether/jobs/api/controller/CategoryController.java
+++ src/main/java/com/risetogether/jobs/api/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.risetogether.jobs.api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.risetogether.jobs.api.service.CategoryService;
 import com.risetogether.jobs.api.util.AppResponseBuilder;
 
+@RestController
 public class CategoryController {
 	
 	private final CategoryService categoryService;
---
+++ src/main/java/com/risetogether/jobs/api/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.risetogether.jobs.api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.risetogether.jobs.api.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+		
+	private final CategoryRepository categoryRepository;
+	private final CategoryMapper categoryMapper;
+	
+	public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.categoryMapper = categoryMapper;
+	}
+	
+	
+}
