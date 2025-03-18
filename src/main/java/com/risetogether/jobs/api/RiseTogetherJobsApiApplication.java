248c5db78253ed11a1451586572ace76dbe0000c
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-18 13:15:38 +0530

    Initial commit

---
+++ src/main/java/com/risetogether/jobs/api/RiseTogetherJobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.risetogether.jobs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseTogetherJobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiseTogetherJobsApiApplication.class, args);
+	}
+
+}
