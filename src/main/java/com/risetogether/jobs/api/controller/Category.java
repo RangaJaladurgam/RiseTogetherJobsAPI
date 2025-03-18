134242cbab085370e7925f68b0e9b90a54219a10
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-18 19:04:12 +0530

    Creating Category Controller

--- src/main/java/com/risetogether/jobs/api/controller/Category.java
+++
@@ -1,5 +0,0 @@
-package com.risetogether.jobs.api.controller;
-
-public class Category {
-
-}
---
+++ src/main/java/com/risetogether/jobs/api/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.risetogether.jobs.api.controller;
+
+import com.risetogether.jobs.api.util.AppResponseBuilder;
+
+public class CategoryController {
+	
+	private final CategoryService categoryService;
+	private final AppResponseBuilder response;
+	
+	public CategoryController(CategoryService categoryService, AppResponseBuilder response) {
+		super();
+		this.categoryService = categoryService;
+		this.response = response;
+	}
+	
+	
+	
+	
+	
+	
+}
