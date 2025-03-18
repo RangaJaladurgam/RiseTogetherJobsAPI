de2c74701c3dc7a40282baec33a82c595dffdf3a
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-18 19:09:26 +0530

    Creating categoryRequest

---
+++ src/main/java/com/risetogether/jobs/api/requestdto/CategoryRequest.java
@@ -0,0 +1,16 @@
+package com.risetogether.jobs.api.requestdto;
+
+public class CategoryRequest {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
