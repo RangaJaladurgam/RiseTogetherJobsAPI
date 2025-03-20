377e961f653a3fbc1670b1bdc1368b2f38e7d9cf
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-20 15:29:47 +0530

    Working on AuthResponse

--- src/main/java/com/risetogether/jobs/api/security/AuthController.java
+++ src/main/java/com/risetogether/jobs/api/security/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.risetogether.jobs.api.requestdto.LoginRequest;
+import com.risetogether.jobs.api.responsedto.AuthResponse;
 
 @RestController
 @RequestMapping("/auth")
