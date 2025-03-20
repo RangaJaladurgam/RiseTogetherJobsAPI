77f44c5603b150bae0fac01a643f258b312a827e
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-20 15:50:29 +0530

    working on Validation

--- src/main/java/com/risetogether/jobs/api/requestdto/AdminRequest.java
+++ src/main/java/com/risetogether/jobs/api/requestdto/AdminRequest.java
@@ -1,9 +1,26 @@
 package com.risetogether.jobs.api.requestdto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class AdminRequest {
 	
+	@NotBlank(message = "username cannot be blank")
+	@NotNull(message = "username cannot be null")
 	private String username;
+	
+	@NotBlank(message = "email cannot be blank")
+	@NotNull(message = "email cannot be null")
+	@Email(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$"
+		  ,message = "Invalid Gmail Id")
 	private String email;
+	
+	@NotNull(message = "password cannot be null")
+	@NotBlank(message = "password cannot be blank")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,12}$"
+		    ,message = "password must have at least one uppercase letter, one lowercase letter, one number, one special character, and a length between 8 and 12 characters")
 	private String password;
 	
 	public String getUsername() {
