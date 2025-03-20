8e71eeee02c95b74621477ccecc41fbd649164dd
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-20 15:44:04 +0530

    Working on PasswordEncoder

--- src/main/java/com/risetogether/jobs/api/security/SecurityConfig.java
+++ src/main/java/com/risetogether/jobs/api/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/register","/auth/login", "/admins/{email}").permitAll()
+                        .requestMatchers("/admins/register","/auth/login", "/admins/{email}").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless API
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class) // Add JWT filter
